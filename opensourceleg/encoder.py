85434f5a7cd30e79ecba5c4d6b9b484c918b8391
sjaxel <axel.sjogren.holtz@gmail.com>
2023-05-03 23:00:00 +0100

    Running codestyle, lint and adjusted type errors

--- opensourceleg/encoder.py
+++ opensourceleg/encoder.py
@@ -1,10 +1,12 @@
 import logging
-from opensourceleg.hardware import UnitsDefinition, DEFAULT_UNITS
-from smbus2 import SMBus
-from smbus2 import I2cFunc
 import time
-import numpy as np
 from abc import ABC, abstractmethod
+
+import numpy as np
+from smbus2 import I2cFunc, SMBus
+
+from opensourceleg.hardware import DEFAULT_UNITS, UnitsDefinition
+
 
 class Encoder(ABC):
     def __init__(
@@ -16,26 +18,28 @@ class Encoder(ABC):
         debug_level: int = 0,
     ) -> None:
         self.debug_level = debug_level
-        if(logger):
+        if logger:
             self.logger = logger.getChild(name)
         else:
             self.logger = logging.getLogger(name)
-            logging.basicConfig(format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s', 
-                                datefmt='%I:%M:%S', 
-                                level=logging.DEBUG)
+            logging.basicConfig(
+                format="[%(asctime)s][%(name)s][%(levelname)s] %(message)s",
+                datefmt="%I:%M:%S",
+                level=logging.DEBUG,
+            )
         self.name = name
         self._state = None
         self._units = units if units else DEFAULT_UNITS
         self._isOpen = False
-        self.max_encoder_counts = encoder_res # 14 bit encoder, so 14 bits!
+        self.max_encoder_counts = encoder_res  # 14 bit encoder, so 14 bits!
 
-        self._encoder_angle = 0 # in encoder counts
-        self.joint_velocity = 0 # in encoder counts/sec
+        self._encoder_angle = 0  # in encoder counts
+        self.joint_velocity = 0  # in encoder counts/sec
         self.last_update_time = 0
 
     def __enter__(self) -> None:
         self.open()
-    
+
     def __exit__(self, type, value, tb) -> None:
         self.close()
 
@@ -45,6 +49,7 @@ class Encoder(ABC):
     Args:
         None
     """
+
     def open(self) -> None:
         if not self._isOpen:
             self._open()
@@ -61,17 +66,17 @@ class Encoder(ABC):
     @abstractmethod
     def _close(self) -> None:
         raise NotImplementedError("Subclass must implement _close() function")
-        
+
     def update(self) -> None:
         if self._isOpen:
             self._update()
         else:
-            raise IOError("Bus needs to be opened before calling update")
+            raise OSError("Bus needs to be opened before calling update")
 
     @abstractmethod
     def _update(self) -> None:
         raise NotImplementedError("Subclass must implement _update function")
-    
+
     @property
     @abstractmethod
     def encoder_position(self):
@@ -82,26 +87,29 @@ class Encoder(ABC):
     def encoder_velocity(self):
         raise NotImplementedError("Subclass must implement encoder_velocity property")
 
-class AS5048A_Encoder(Encoder):
-    ENC_RESOLUTION = 2**14 # 14 bit resolution
 
-    I2C_BASE_ADR_7BIT = 0b1000000 # The adress base on the format <base[6:2]> <A1[1]> <A2[0]>
+class AS5048A_Encoder(Encoder):
+    ENC_RESOLUTION = 2**14  # 14 bit resolution
+
+    I2C_BASE_ADR_7BIT = (
+        0b1000000  # The adress base on the format <base[6:2]> <A1[1]> <A2[0]>
+    )
 
     ## Register adresses I2C
-    OTP_ZERO_POSITION_HIGH = 0x16 # bit 13 through 6
-    OTP_ZERO_POSITION_LOW = 0x17 # bit 5 through 0 (2 msbs of this aren't used)
-    AUTOMATIC_GAIN_CONTROL = 0xFA # 0 = high mag field, 255 = low mag field, 8 bit
-    DIAGNOSTICS = 0xFB # flags: 3 = comp high, 2 = comp low, 1 = COF, 0 = OCF
-    MAGNITUDE_HIGH = 0xFC # bit 13 through 6
-    MAGNITUDE_LOW = 0xFD # bit 5 through 0 (2 msbs of this aren't used)
-    ANGLE_HIGH = 0xFE # bit 13 through 6
-    ANGLE_LOW = 0xFF # bit 5 through 0 (2 msbs of this aren't used)
+    OTP_ZERO_POSITION_HIGH = 0x16  # bit 13 through 6
+    OTP_ZERO_POSITION_LOW = 0x17  # bit 5 through 0 (2 msbs of this aren't used)
+    AUTOMATIC_GAIN_CONTROL = 0xFA  # 0 = high mag field, 255 = low mag field, 8 bit
+    DIAGNOSTICS = 0xFB  # flags: 3 = comp high, 2 = comp low, 1 = COF, 0 = OCF
+    MAGNITUDE_HIGH = 0xFC  # bit 13 through 6
+    MAGNITUDE_LOW = 0xFD  # bit 5 through 0 (2 msbs of this aren't used)
+    ANGLE_HIGH = 0xFE  # bit 13 through 6
+    ANGLE_LOW = 0xFF  # bit 5 through 0 (2 msbs of this aren't used)
 
     ## Status flags Diagnostics registers
-    FLAG_COMP_H = (0x1 << 3)
-    FLAG_COMP_L = (0x1 << 2)
-    FLAG_COF    = (0x1 << 1)
-    FLAG_OCF    = (0x1 << 0)
+    FLAG_COMP_H = 0x1 << 3
+    FLAG_COMP_L = 0x1 << 2
+    FLAG_COF = 0x1 << 1
+    FLAG_OCF = 0x1 << 0
 
     """Class for the AS5048A encoder, implements the Encoder interface
 
@@ -124,54 +132,56 @@ class AS5048A_Encoder(Encoder):
     Returns:
         _type_: _description_
     """
+
     def __init__(
-        self, 
-        bus: str = '/dev/i2c',
-        A1_adr_pin: int = 0b0,
-        A2_adr_pin: int = 0b0,
+        self,
+        bus: str = "/dev/i2c",
+        A1_adr_pin: bool = False,
+        A2_adr_pin: bool = False,
         name: str = "AS5048A_Encoder",
         units: UnitsDefinition = DEFAULT_UNITS,
         logger: logging.Logger = None,
         debug_level: int = 0,
     ) -> None:
-        super().__init__(AS5048A_Encoder.ENC_RESOLUTION, 
-                        name, 
-                        units, 
-                        logger, 
-                        debug_level
+        super().__init__(
+            AS5048A_Encoder.ENC_RESOLUTION, name, units, logger, debug_level
         )
         self.bus = bus
-        self._SMBus = None
         self.addr = AS5048A_Encoder._calculateI2CAdress(A1_adr_pin, A2_adr_pin)
 
-        self._encData_old = None
-        self._encData_old_timestamp = None    
-        self._encData_new = None
-        self._encData_new_timestamp = None
-
+        self._encData_old = bytes(6)
+        self._encData_old_timestamp = 0
+        self._encData_new = bytes(6)
+        self._encData_new_timestamp = 0
 
     def _open(self) -> None:
         self._SMBus = SMBus(self.bus)
-        self.logger.debug(f"[OPEN] SMBUS func: {self._SMBus.funcs:>08x} \n\r" + str(I2cFunc(self._SMBus.funcs)))
+        self.logger.debug(
+            f"[OPEN] SMBUS func: {self._SMBus.funcs:>08x} \n\r"
+            + str(I2cFunc(self._SMBus.funcs))
+        )
         self._update()
         self._isOpen = True
 
     def _close(self) -> None:
-        self._SMBus.close()
+        if hasattr(self, "_SMBus"):
+            self._SMBus.close()
         self._isOpen = False
 
+    @staticmethod
     def _calculateI2CAdress(a1: bool, a2: bool) -> int:
         # (a1, a2) = adress_pins
         return AS5048A_Encoder.I2C_BASE_ADR_7BIT | ((bool(a2)) << 1) | ((bool(a1)) << 0)
-    
+
+    @staticmethod
     def _get14Bit(bytesToParse: bytes) -> int:
         return int((bytesToParse[0] << 6) | bytesToParse[1])
-    
+
     def _readRegisters(self, register, len) -> bytes:
         return bytes(self._SMBus.read_i2c_block_data(self.addr, register, len))
 
     def _readDataRegisters(self) -> None:
-        """  
+        """
         Read data output registers
             [0]
             [1]
@@ -183,21 +193,22 @@ class AS5048A_Encoder(Encoder):
         self._encData_old = self._encData_new
         self._encData_old_timestamp = self._encData_new_timestamp
 
-        self._encData_new = self._readRegisters(AS5048A_Encoder.AUTOMATIC_GAIN_CONTROL, 6)
+        self._encData_new = self._readRegisters(
+            AS5048A_Encoder.AUTOMATIC_GAIN_CONTROL, 6
+        )
         self._encData_new_timestamp = time.monotonic_ns()
 
-
     def _checkDiagnostics(self):
-        if(not self.diag_OCF):
-            raise IOError("Invalid data returned on read, DIAG_OCF != 1")
-        
-        if(self.diag_COF):
+        if not self.diag_OCF:
+            raise OSError("Invalid data returned on read, DIAG_OCF != 1")
+
+        if self.diag_COF:
             self.logger.error("CORDIC Overflow, sample invalid")
 
-        if(self.diag_compH):
+        if self.diag_compH:
             self.logger.warning("Low magnetic field comp triggered")
 
-        if(self.diag_compL):
+        if self.diag_compL:
             self.logger.warning("High magnetic field comp triggered")
 
     def _update(self):
@@ -216,7 +227,7 @@ class AS5048A_Encoder(Encoder):
             float
         """
         encAngleData = AS5048A_Encoder._get14Bit(self._encData_new[4:6])
-        return (encAngleData * 2 * np.pi)  / self.max_encoder_counts
+        return (encAngleData * 2 * np.pi) / self.max_encoder_counts
 
     @property
     def encoder_velocity(self) -> float:
@@ -233,15 +244,19 @@ class AS5048A_Encoder(Encoder):
             encAngleDataOld = AS5048A_Encoder._get14Bit(self._encData_old[4:6])
             encAngleDataNew = AS5048A_Encoder._get14Bit(self._encData_new[4:6])
             # Timediff is converted from ns to s (x10^-9)
-            timediff = (self._encData_new_timestamp - self._encData_old_timestamp) * 10**-9
+            timediff = (
+                self._encData_new_timestamp - self._encData_old_timestamp
+            ) * 10**-9
         except TypeError:
             ## Typeerror indicate we only took one sample and have 0 velocity.
-            return 0
+            return float(0)
         else:
-            return (encAngleDataNew - encAngleDataOld) \
-                    * (2 * np.pi) \
-                    / self.max_encoder_counts \
-                    / (timediff)
+            return (
+                (encAngleDataNew - encAngleDataOld)
+                * (2 * np.pi)
+                / self.max_encoder_counts
+                / (timediff)
+            )
 
     @property
     def diag_compH(self) -> bool:
@@ -251,9 +266,9 @@ class AS5048A_Encoder(Encoder):
 
         Returns:
             Status of COMP_H diagnostics flag
-        """        
-        return self._encData_new[1] & AS5048A_Encoder.FLAG_COMP_H
-    
+        """
+        return bool(self._encData_new[1] & AS5048A_Encoder.FLAG_COMP_H)
+
     @property
     def diag_compL(self) -> bool:
         """
@@ -263,9 +278,9 @@ class AS5048A_Encoder(Encoder):
 
         Returns:
             Status of COMP_L diagnostics flag
-        """        
-        return self._encData_new[1] & AS5048A_Encoder.FLAG_COMP_L
-    
+        """
+        return bool(self._encData_new[1] & AS5048A_Encoder.FLAG_COMP_L)
+
     @property
     def diag_COF(self) -> bool:
         """
@@ -275,9 +290,9 @@ class AS5048A_Encoder(Encoder):
         maintains the last valid angular value.
         Returns:
             Status of COF diagnostics flag
-        """        
-        return self._encData_new[1] & AS5048A_Encoder.FLAG_COF
-    
+        """
+        return bool(self._encData_new[1] & AS5048A_Encoder.FLAG_COF)
+
     @property
     def diag_OCF(self) -> bool:
         """
@@ -287,9 +302,9 @@ class AS5048A_Encoder(Encoder):
 
         Returns:
             Status of OCF diagnostics flag
-        """        
-        return self._encData_new[1] & AS5048A_Encoder.FLAG_OCF
-                
+        """
+        return bool(self._encData_new[1] & AS5048A_Encoder.FLAG_OCF)
+
     def __repr__(self) -> str:
         ang = self.encoder_position
         vel = self.encoder_velocity
@@ -297,8 +312,8 @@ class AS5048A_Encoder(Encoder):
         return str
 
 
-if __name__ == '__main__':
-    enc = AS5048A_Encoder('/dev/i2c-1')
+if __name__ == "__main__":
+    enc = AS5048A_Encoder("/dev/i2c-1")
     enc.open()
     with enc:
         while True:
--- tests/test_hardware/test_encoder.py
+++ tests/test_hardware/test_encoder.py
@@ -1,39 +1,41 @@
 import pytest
 import pytest_mock
+from numpy import pi
 from pytest_mock import mocker
+from smbus2 import SMBus
 
 from opensourceleg.encoder import AS5048A_Encoder
-from smbus2 import SMBus
-from numpy import pi 
+
 
 @pytest.fixture
 def enc_obj(mock_smbus):
     "Test docstring"
-    obj = AS5048A_Encoder(bus='/dev/null')
+    obj = AS5048A_Encoder(bus="/dev/null")
     return obj
 
+
 @pytest.fixture
-def enc_obj_open(mocker: pytest_mock, enc_obj: AS5048A_Encoder):
+def enc_obj_open(mocker, enc_obj: AS5048A_Encoder):
     mock_i2c_return_data(mocker, bytes(range(6)))
     enc_obj.open()
     return enc_obj
+
 
 def mock_i2c_return_data(mocker, data: bytes):
     m = mocker.patch("smbus2.SMBus.read_i2c_block_data")
     m.return_value = data
     return m
 
-def test_AS5048A_Encoder(enc_obj: AS5048A_Encoder, 
-                        enc_obj_open: AS5048A_Encoder
-    ):
+
+def test_AS5048A_Encoder(enc_obj: AS5048A_Encoder, enc_obj_open: AS5048A_Encoder):
     assert type(enc_obj) == AS5048A_Encoder
-    assert enc_obj_open._SMBus.funcs == 0x0eff0009
+    assert enc_obj_open._SMBus.funcs == 0x0EFF0009
     assert enc_obj_open._isOpen == True
-    
+
 
 @pytest.fixture()
 def mock_smbus(monkeypatch, block_data=None):
-    SMBUS_FUNCS = 0x0eff0009 
+    SMBUS_FUNCS = 0x0EFF0009
     """
     Mock function for the SMBus I2C interface
 
@@ -50,37 +52,34 @@ def mock_smbus(monkeypatch, block_data=None):
 
     def mock_open(self, bus):
         self.funcs = SMBUS_FUNCS
+
     monkeypatch.setattr(SMBus, "open", mock_open)
+
 
 @pytest.mark.parametrize(
     ("data, exp_pos"),
     [
         (bytes([0, 0xFF, 0, 0, 0, 0]), 0),
-        (bytes([0, 0xFF, 0, 0, 0xFF, 0x3F]), 2*pi),
+        (bytes([0, 0xFF, 0, 0, 0xFF, 0x3F]), 2 * pi),
         (bytes([0, 0xFF, 0, 0, 0x7F, 0x3F]), pi),
-        (bytes([0, 0xFF, 0, 0, 0x6F, 0x3F]), pi/2)
-    ]
+        (bytes([0, 0xFF, 0, 0, 0x6F, 0x3F]), pi / 2),
+    ],
 )
 def test_update_pos(mocker, enc_obj_open: AS5048A_Encoder, data: bytes, exp_pos: float):
     m = mock_i2c_return_data(mocker, data)
     enc_obj_open.update()
-    assert enc_obj_open.encoder_position == pytest.approx(exp_pos, 
-                                                            rel=AS5048A_Encoder.ENC_RESOLUTION, 
-                                                            )
+    assert enc_obj_open.encoder_position == pytest.approx(
+        exp_pos,
+        rel=AS5048A_Encoder.ENC_RESOLUTION,
+    )
 
-   
-@pytest.mark.parametrize(
-    ("test_data"),
-    [
-        bytes(range(5)),
-        b'',
-        bytes(range(100))
-    ]
-)
+
+@pytest.mark.parametrize(("test_data"), [bytes(range(5)), b"", bytes(range(100))])
 def test_readRegisters(mocker, enc_obj_open: AS5048A_Encoder, test_data: bytes):
     m = mock_i2c_return_data(mocker, test_data)
     assert enc_obj_open._isOpen == True
     assert enc_obj_open._readRegisters(5, len(bytes(test_data))) == test_data
+
 
 @pytest.mark.parametrize(
     ("a1, a2, expected"),
@@ -91,8 +90,8 @@ def test_readRegisters(mocker, enc_obj_open: AS5048A_Encoder, test_data: bytes):
         (1, 1, 0b1000011),
         (True, True, 0b1000011),
         (100, 100, 0b1000011),
-        (100, False, 0b1000001)
-    ]
+        (100, False, 0b1000001),
+    ],
 )
 def test_calculateI2CAdress(a1, a2, expected):
     assert AS5048A_Encoder._calculateI2CAdress(a1, a2) == expected
